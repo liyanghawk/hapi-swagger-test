0ef9da1ee10553020ed1d81325bcd3912f1d6316
liyang <liyang@bonc.com.cn>
2017-07-18 11:11:56 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const Hapi = require('hapi');
+const Inert = require('inert');
+const Vision = require('vision');
+const HapiSwagger = require('hapi-swagger');
+const Pack = require('./package');
+const Joi = require('joi');
+
+const server = new Hapi.Server();
+server.connection({
+        host: 'localhost',
+        port: 3000
+    });
+
+const options = {
+    info: {
+            'title': 'Test API Documentation',
+            'version': Pack.version,
+        }
+    };
+
+server.register([
+    Inert,
+    Vision,
+    {
+        'register': HapiSwagger,
+        'options': options
+    }], (err) => {
+        server.start( (err) => {
+           if (err) {
+                console.log(err);
+            } else {
+                console.log('Server running at:', server.info.uri);
+            }
+        });
+    });
+
+server.route({
+    method: 'GET',
+    path: '/test',
+    config: {
+        handler: (request, reply) => { reply('OK'); },
+        description: 'Get todo',
+        notes: 'Returns a todo item by the id passed in the path',
+        tags: ['api'], // ADD THIS TAG
+    },
+});
